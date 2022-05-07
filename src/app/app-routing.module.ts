b149d0743cc0e97682dd9c13c1645f23f5f21202
n1c02001 <nicorincon26@gmail.com>
2022-05-07 19:44:28 -0300

    primero

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,49 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Router, RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './componentes/header/header.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { SobremiComponent } from './componentes/sobremi/sobremi.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { ConocimientosComponent } from './componentes/conocimientos/conocimientos.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    component: InicioComponent ,
+},
+  {
+    path: 'Inicio',
+    component: InicioComponent ,
+},
+{
+  path: 'Sobremi',
+  component: SobremiComponent ,
+},
+{
+path: 'Experiencia',
+  component: ExperienciaComponent ,
+},
+{
+  path: 'Educacion',
+    component: EducacionComponent ,
+},
+{
+    path: 'Proyectos',
+      component: ProyectosComponent ,
+},
+{
+  path: 'Lenguajes',
+    component: ConocimientosComponent ,
+},
+{
+  path: 'Contacto',
+    component: ContactoComponent ,
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { SobremiComponent } from './componentes/sobremi/sobremi.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { ConocimientosComponent } from './componentes/conocimientos/conocimientos.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    InicioComponent,
+    SobremiComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    ProyectosComponent,
+    ConocimientosComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
